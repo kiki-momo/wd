86061e610ccfe6530f382908b62648b99f4e0604
kiki-momo <163088983+kiki-momo@users.noreply.github.com>
2024-04-02 15:38:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+let startTime;
+let updatedTime;
+let difference;
+let tInterval;
+let running = false;
+
+function startTimer() {
+    if (!running) {
+        startTime = new Date().getTime();
+        tInterval = setInterval(getShowTime, 1000);
+        running = true;
+    }
+}
+
+function pauseTimer() {
+    if (running) {
+        clearInterval(tInterval);
+        running = false;
+    }
+}
+
+function resetTimer() {
+    clearInterval(tInterval);
+    running = false;
+    document.getElementById('timer').innerHTML = '00:00:00';
+}
+
+function getShowTime() {
+    updatedTime = new Date().getTime();
+    difference = updatedTime - startTime;
+
+    let hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    document.getElementById('timer').innerHTML =
+        (hours < 10 ? "0" + hours : hours) + ":" +
+        (minutes < 10 ? "0" + minutes : minutes) + ":" +
+        (seconds < 10 ? "0" + seconds : seconds);
+}
